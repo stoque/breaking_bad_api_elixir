1426c56fc35ed2028dfb0e2e6e5ab1b49aaffa68
Lucas Stoque <lucas.stoque@ifood.com.br>
2022-02-04 21:06:11 -0300

    add defdelegates

--- lib/breaking_bad_api_elixir.ex
+++ lib/breaking_bad_api_elixir.ex
@@ -1,18 +1,21 @@
 defmodule BreakingBadApiElixir do
-  @moduledoc """
-  Documentation for `BreakingBadApiElixir`.
-  """
+  defdelegate get_all_characters(),
+    to: BreakingBadApiElixir.Api.Characters.Get,
+    as: :get_all_characters
+  defdelegate get_character_by_id(id),
+    to: BreakingBadApiElixir.Api.Characters.Get,
+    as: :get_character_by_id
+  defdelegate get_random_character(),
+    to: BreakingBadApiElixir.Api.Characters.Get,
+    as: :get_random_character
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> BreakingBadApiElixir.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  defdelegate get_all_episodes(),
+    to: BreakingBadApiElixir.Api.Episodes.Get,
+    as: :get_all_episodes
+  defdelegate total_episodes(),
+    to: BreakingBadApiElixir.Api.Episodes.Get,
+    as: :total_episodes
+  defdelegate get_random_episode(),
+    to: BreakingBadApiElixir.Api.Episodes.Get,
+    as: :get_random_episode
 end
